5e74562d3bc57a53fc913a35865f07f04b734d3d
craig wadding <craig.wadding@gmail.com>
2019-05-20 00:37:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
-def my_hash{something}
-  something = "dogs", 
+def my_hash
+  my_hash={"bootcamp" => "Flatiron", "start_date" => "Monday", "progress" => "week_one"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
